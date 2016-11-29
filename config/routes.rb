74ec2067de5f01f071c1af5abae025c53671d880
Brad <bgreen3@uoregon.edu>
2016-11-29 15:15:44 -0700

    Add items relationship endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,11 @@ Rails.application.routes.draw do
       resources :customers, except: [:new, :edit]
       resources :transactions, except: [:new, :edit]
       resources :invoices, except: [:new, :edit]
-      resources :items, except: [:new, :edit]
       resources :invoice_items, except: [:new, :edit]
+      resources :items, except: [:new, :edit] do
+        get '/invoice_items', to: 'items/invoice_items#index'
+        get '/merchant', to: 'items/merchants#show'
+      end
     end
   end
 end
