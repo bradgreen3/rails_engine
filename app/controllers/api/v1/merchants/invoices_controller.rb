50f8f2d53086c2ad10647833ed3848b8f614e585
Matt D <MDes41@users.noreply.github.com>
2016-11-29 16:16:33 -0700

    Add relationship endpoints for customers, transactions, and merchants

---
+++ app/controllers/api/v1/merchants/invoices_controller.rb
@@ -0,0 +1,5 @@
+class Api::V1::Merchants::InvoicesController < ApplicationController
+  def index
+    render json: Merchant.find(params[:merchant_id]).invoices
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :merchants, except: [:new, :edit]
-      resources :customers, except: [:new, :edit]
-      resources :transactions, except: [:new, :edit]
+      resources :merchants, except: [:new, :edit] do
+        get '/items', to: 'merchants/items#index'
+        get '/invoices', to: 'merchants/invoices#index'
+      end
+      resources :customers, except: [:new, :edit] do
+        get '/invoices', to: 'customers/invoices#index'
+        get '/transactions', to: 'customers/transactions#index'
+        end
+      resources :transactions, except: [:new, :edit] do
+        get '/invoice', to: 'transactions/invoices#show'
+      end
       resources :invoices, except: [:new, :edit]
       resources :items, except: [:new, :edit]
       resources :invoice_items, except: [:new, :edit]
