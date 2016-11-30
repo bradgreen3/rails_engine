bc1fa746fe69acc53dc7e106633b9e5f124e3ca3
Matt D <MDes41@users.noreply.github.com>
2016-11-29 20:02:55 -0700

    Add invoice items search controllers

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,11 @@ Rails.application.routes.draw do
         get 'find_all', to: 'search#index'
         get 'random', to: 'random#show'
       end
+      namespace :invoice_items do
+        get 'find', to: 'search#show'
+        get 'find_all', to: 'search#index'
+        get 'random', to: 'random#show'
+      end
       resources :merchants, except: [:new, :edit] do
         scope module: 'merchants' do
           get 'items', to: 'items#index'
