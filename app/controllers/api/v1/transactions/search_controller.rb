241369952feec3d6f3db4757aa6ec956f204aefa
Brad <bgreen3@uoregon.edu>
2016-11-29 19:36:44 -0700

    Add find, find_all, and random search for transactions

---
+++ app/controllers/api/v1/transactions/search_controller.rb
@@ -0,0 +1,15 @@
+class Api::V1::Transactions::SearchController < ApplicationController
+  def show
+    render json: Transaction.find_by(search_params)
+  end
+
+  def index
+    render json: Transaction.where(search_params)
+  end
+
+  private
+
+  def search_params
+    params.permit(:id, :invoice_id, :credit_card_number, :credit_card_expiration_date, :result, :created_at, :updated_at)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,11 @@ Rails.application.routes.draw do
         get 'find_all', to: 'search#index'
         get 'random', to: 'random#show'
       end
+      namespace :transactions do
+        get 'find', to: 'search#show'
+        get 'find_all', to: 'search#index'
+        get 'random', to: 'random#show'
+      end
       resources :merchants, except: [:new, :edit] do
         get '/items', to: 'merchants/items#index'
         get '/invoices', to: 'merchants/invoices#index'
