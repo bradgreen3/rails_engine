31c3f9d804bd89bb4890eee80f5f49e6a2c17f5c
Brad Green <bgreen3@uoregon.edu>
2016-12-01 11:00:59 -0700

    Merchant rev date (#25)
    * Add single customer business intelligence and test
    
    * Add revenue for merchant by invoice date

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -3,13 +3,24 @@ class Merchant < ApplicationRecord
   has_many :invoices
   has_many :customers, through: :invoices
 
-  def total_revenue
-  	{revenue: dollarize(self.invoices.joins(:transactions, :invoice_items)
-														.where(transactions: {result: 'success'})
-														.sum('invoice_items.quantity * invoice_items.unit_price')
-												)}
+  def total_revenue(date=nil)
+    date == nil ? tr_no_date : tr_has_date(date)
   end
 
+  def tr_no_date
+    {revenue: dollarize(self.invoices.joins(:transactions, :invoice_items)
+                            .where(transactions: {result: 'success'})
+                            .sum('invoice_items.quantity * invoice_items.unit_price')
+                        )}
+  end
+
+  def tr_has_date(date)
+    {revenue: dollarize(self.invoices.joins(:transactions, :invoice_items)
+                            .where(transactions: {result: 'success'}, invoices: {created_at: date})
+                            .sum('invoice_items.quantity * invoice_items.unit_price')
+                        )}
+  end
+  
   def self.favorite_merchant(customer_id)
     joins(invoices: :transactions)
     .where(transactions: {result: 'success'}, invoices: {customer_id: customer_id})
--- spec/requests/api/v1/single_merchant_business_intelligence_spec.rb
+++ spec/requests/api/v1/single_merchant_business_intelligence_spec.rb
@@ -47,4 +47,27 @@ describe 'Single merchant business intelligence' do
 		end
 	end
 
+	context 'revenue' do
+		it 'calculated the revenue for one merchant for specific invoice date' do
+			merchant = create(:merchant)
+			invoice1 = create(:invoice, merchant: merchant, created_at: "2001-01-01 12:00:00")
+			invoice2 = create(:invoice, merchant: merchant, created_at: "2001-01-01 12:00:00")
+			invoice3 = create(:invoice, merchant: merchant, created_at: "1990-01-20 5:00:00")
+			transaction = create(:transaction, invoice: invoice1, result: 'success')
+			transaction = create(:transaction, invoice: invoice2, result: 'success')
+			transaction = create(:transaction, invoice: invoice3, result: 'success')
+			transaction = create(:transaction, invoice: invoice2, result: 'failed')
+			invoice_item = create(:invoice_item, invoice: invoice1, quantity: 10, unit_price: 500)
+			invoice_item = create(:invoice_item, invoice: invoice2, quantity: 10, unit_price: 1000)
+			invoice_item = create(:invoice_item, invoice: invoice3, quantity: 10, unit_price: 500)
+
+			date = "2001-01-01 12:00:00"
+			get "/api/v1/merchants/#{merchant.id}/revenue?date=#{date}"
+
+			merchant_response = JSON.parse(response.body)
+
+			expect(merchant_response['revenue']).to eq('150.00')
+		end
+	end
+
 end
